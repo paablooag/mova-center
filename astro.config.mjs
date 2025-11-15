674e9d14fd1bf64bf5c9b8acb6f68b771d61adfc
Pablo <pablo.alcalde.garcia@gmail.com>
2025-11-15 15:27:29 +0100

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://tu-subdominio.com', // Actualiza con tu URL real
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ],
 });
 
